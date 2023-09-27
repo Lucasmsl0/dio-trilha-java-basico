1822f4552fe86d4b2cc3d92202d7f30049829305
Lucasmsl0 <lucaslimamx0@gmail.com>
2023-09-27 20:27:06 -0300

    Update TiposVariaveis.java
    Segundo commit com alguns instruções sobre tipos e variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,16 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        int salarioMinimo = 2500; // Na linguagem java a virgula é representada pelo "." pois o java se baseia no valor americano   
+        double numeroExemplo = 2.500; //Observe que o número não vai contar os zeros depois do 5 pois ele é considerado um decimal assim ignorando os zeros
+        
+        System.out.println("\n" + salarioMinimo);
+        System.out.println(numeroExemplo + "\n");
+        
+        int numero = 3;
+        numero = 10;
+        final double VALOR_DE_PI = 3.14;
+        // Perceba que o valor da variavel numero mudou, já o de PI não mudou pois adicionando "final" antes do tipo da variavel indica ao java que ela é uma constante
+        System.out.println(numero);
+        System.out.println(VALOR_DE_PI);
     }
 }
